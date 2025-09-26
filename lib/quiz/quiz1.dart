afb529fdd816fc9223b53ee0601b6fd2a32f1077
Arprnto <0204ariaprianto@gmail.com>
2025-09-26 19:47:33 +0700

    masih membuat layout

--- lib/quiz/quiz1.dart
+++ lib/quiz/quiz1.dart
@@ -9,20 +9,62 @@ class Quiz1 extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        color: Colors.amber,
+        color: const Color.fromARGB(0, 158, 158, 158),
         child: Column(
           children: [
             Container(
               margin: EdgeInsets.only(top: 70),
               width: 500,
               height: 100,
-              color: Colors.blue,
-              child: Container(
-                margin: EdgeInsets.only(top: 40),
-                child: Text("Training", style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),),
+              // color: Colors.white,
+              child: Column(
+                 crossAxisAlignment:  CrossAxisAlignment.start,
+                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Text("Training", style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                    ),),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: Text("Your Program", style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold
+                        ),),
+                      ),
+                      Container(
+                        child: Text("Details >", style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue
+                        ),),
+                      ),
+                    ],
+                  ),
+
+                ],
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              width: 500,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.purple.shade400,
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [Colors.purple, Colors.purpleAccent, Colors.white])
+                ),
+              child: Column(
+                children: [
+                  Text("Next Workout")
+                ],
               ),
             )
           ],
