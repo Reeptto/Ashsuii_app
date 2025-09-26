50c129defd63c5cb33c709092d3c9ba96d5d5a07
Arprnto <0204ariaprianto@gmail.com>
2025-09-26 19:30:03 +0700

    membuat layout

--- lib/quiz/quiz1.dart
+++ lib/quiz/quiz1.dart
@@ -6,7 +6,28 @@ class Quiz1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.amber,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 70),
+              width: 500,
+              height: 100,
+              color: Colors.blue,
+              child: Container(
+                margin: EdgeInsets.only(top: 40),
+                child: Text("Training", style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+                ),),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
